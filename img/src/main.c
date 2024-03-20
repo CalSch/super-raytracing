6ea42dfc7e3a81e24fbb1fd95e92c440edddee72
CalSch <cal.schwick@gmail.com>
2024-03-19 22:21:35 -0500

    added a bmp writer for the image renderer

--- img/src/main.c
+++ img/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "rtx_manager.h"
+#include "libbmp.h"
 
 RTXManager rtx;
 
@@ -18,10 +19,29 @@ int writeToPPM(const char *filename, RTXManager r) {
     return 0;
 }
 
+int writeToBMP(char *filename, RTXManager r) {
+    bmp_img bmp;
+    bmp_img_init_df(&bmp, r.width, r.height);
+
+    for (int y=0;y<r.height;y++) {
+        for (int x=0;x<r.width;x++) {
+            int i = x + y*r.width;
+            RGB c = r.buf1[i];
+            bmp_pixel_init(&bmp.img_pixels[y][x],c.r,c.g,c.b);
+        }
+    }
+
+    
+	bmp_img_write(&bmp, filename);
+	bmp_img_free(&bmp);
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     printf("Hello world\n");
     int width = 200;
     int height = 200;
+    int raysPerPixel = 20;
     for (int i=0;i<argc;i++) {
         printf("argv[%d]=\"%s\"\n",i,argv[i]);
         switch (i)
@@ -34,16 +54,24 @@ int main(int argc, char *argv[]) {
             height = atoi(argv[i]);
             printf("\theight=%d\n",height);
             break;
+        case 3:
+            raysPerPixel = atoi(argv[i]);
+            printf("\trays per pixel=%d\n",raysPerPixel);
+            break;
         
         default:
             break;
         }
     }
     rtx = makeRTXManager(width,height);
+    rtx.config.raysPerPixel = raysPerPixel;
     printf("RTX init\n");
     RTXRender(&rtx);
     printf("Rendered\n");
-    writeToPPM("thing.ppm",rtx);
+
+    // writeToPPM("thing.ppm",rtx);
+    writeToBMP("out.bmp",rtx);
+
     printf("done!\n");
     return 0;
 }
