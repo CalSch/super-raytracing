b74e7e69962ea67655263b8d1a0ef58ea53e0037
CalSch <calschwick@calschwick.net>
2025-02-23 22:48:09 -0600

    type casting fix

--- base/src/scene.c
+++ base/src/scene.c
@@ -18,7 +18,7 @@ void sceneAddObject(Scene *scene, Object o) {
     scene->objectCount++;
 
     // Reallocate the objects
-    Object *newObjects = realloc(scene->objects, sizeof(Object) * scene->objectCount);
+    Object *newObjects = (Object*)realloc(scene->objects, sizeof(Object) * scene->objectCount);
     
     // Set the last object in the list to be the new one
     newObjects[scene->objectCount-1] = o;
