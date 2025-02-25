17e1a36c5f844ac41f8e6a54be5613ba7fd0f2ca
CalSch <calschwick@calschwick.net>
2025-02-24 20:13:47 -0600

    added cam rotation

--- cardputer/cardputer.ino
+++ cardputer/cardputer.ino
@@ -95,6 +95,14 @@ void loop() {
       rtx.cam.transform.pos=vec3Add(rtx.cam.transform.pos,vec3Scale(rtx.cam.transform.right,-2.0));
     if (c=='d')
       rtx.cam.transform.pos=vec3Add(rtx.cam.transform.pos,vec3Scale(rtx.cam.transform.right,2.0));
+    if (c==';') // up arrow kinda
+      rotateTransform(&rtx.cam.transform,rtx.cam.transform.right,-0.1);
+    if (c=='.') // down arrow kinda
+      rotateTransform(&rtx.cam.transform,rtx.cam.transform.right,0.1);
+    if (c==',') // left arrow kinda
+      rotateTransform(&rtx.cam.transform,rtx.cam.transform.up,-0.1);
+    if (c=='/') // right arrow kinda
+      rotateTransform(&rtx.cam.transform,rtx.cam.transform.up,0.1);
     rerender=true;
   }
   if (rerender) {
